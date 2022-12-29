adc49da43e4918b0ef3f5c79d6ecefd4d10a265b
aylinozbek <121582241+aylinozbek@users.noreply.github.com>
2022-12-29 22:04:11 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            x = 21;
+            y = 30;
+            int topla = 0; 
+            topla = topla + (x * y);
+            Console.WriteLine(topla);
+
+        }
+    }
+}
